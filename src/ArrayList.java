26a1178646c21242befbfcfd25fd98aaef102c6c
RadoSlav <radoslav4096@gmail.com>
2022-08-31 00:59:23 +0300

    Initial commit. Add methods' declarations

---
+++ src/ArrayList.java
@@ -0,0 +1,63 @@
+
+public class ArrayList <T> {
+	private final int startingCapacity = 5;
+	private T[] arr;
+	private int size;
+	private int capacity;
+	
+	public ArrayList(){
+		//arr = new T[];
+	}
+	
+	public ArrayList(int size) {
+		
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	public boolean empty() {
+		return this.size == 0;
+	}
+
+	public void clear() {
+		
+	}
+	
+	public int capacity() {
+		return this.capacity;
+	}
+	
+	public void reserve(int capacity) {
+		
+	}
+	
+	public void resize(int capacity) {
+		
+	}
+	
+	public void shrink() {
+		
+	}
+	
+	public boolean add(T newElement) {
+		return true;
+	}
+	
+	public void remove() {
+		
+	}
+	
+	public T get(int position) {
+		return null;
+	}
+	
+	public void set(int position, T newValue) {
+		
+	}
+	
+	public void concatenate(ArrayList<T> other) {
+		
+	}
+}
