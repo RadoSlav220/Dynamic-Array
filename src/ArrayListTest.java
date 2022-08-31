e3118bc4a1bff09b0b9853360d3b099138880fc1
RadoSlav <radoslav4096@gmail.com>
2022-08-31 12:09:22 +0300

    Add tests for size() and empty()

--- src/ArrayListTest.java
+++ src/ArrayListTest.java
@@ -23,6 +23,45 @@ class ArrayListTest {
 		assertTrue(list.empty(), "Initial state must be empty");
 	}
 	
-	
+	void testSize() {
+		ArrayList <Integer> list1 = new ArrayList<>();
+		assertEquals(0, list1.size(), "Initial size must be 0");
+		
+		list1.add(6);
+		assertEquals(1, list1.size(), "Size must be 1");
+		
+		list1.add(10);
+		list1.add(4);
+		assertEquals(3, list1.size(), "Size must be 3");
+		
+		list1.remove();
+		assertEquals(2, list1.size(), "Size must be 2 after removing");
+		
+		ArrayList <Integer> list2 = new ArrayList<>();
+		list2.add(4);
+		list2.add(8);
+		list2.add(14);
+		list1.concatenate(list2);
+		assertEquals(5, list1.size(), "Size must be 5 after concatenation");
+		
+		list1.clear();
+		assertEquals(0, list1.size(), "Size must be 0 after clearing");
+	}
 
+	@Test
+	void testEmpty() {
+		ArrayList <String> names = new ArrayList<>();
+		assertTrue(names.empty(), "Initial state must be empty");
+		
+		names.add("Radoslav");
+		names.add("Kaloyan");
+		assertFalse(names.empty(), "There are elements, it must not be empty");
+		
+		names.remove();
+		assertFalse(names.empty(), "There is still an element");
+		
+		names.add("Kristina");
+		names.clear();
+		assertTrue(names.empty(), "Must be empty after clear");
+	}
 }
