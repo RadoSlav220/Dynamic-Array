d85759a759019e872c5819f4daead43f645058ad
RadoSlav <radoslav4096@gmail.com>
2022-09-08 11:55:13 +0300

    Documented ArrayList class

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,45 +1,81 @@
+/**
+ * Dynamic Array (ArrayList in Java)
+ * 
+ * Digital Lights Masterclass 2022
+ * @author Radoslav Ivanov
+ * 
+ * @param <T> Type of the stored data.
+ */
+
 public class ArrayList <T> {
 	public static final int INITIAL_CAPACITY = 5;
-	private T[] arr;
+	private T[] array;
 	private int size;
 	private int capacity;
 	
+	
+	/**
+	 * Construct a new empty ArrayList with initial capacity.
+	 */
 	@SuppressWarnings("unchecked")
 	public ArrayList(){
-		this.arr = (T[]) new Object [INITIAL_CAPACITY];
+		this.array = (T[]) new Object [INITIAL_CAPACITY];
 		this.size = 0;
 		this.capacity = INITIAL_CAPACITY;
 	}
 	
+	
+	/**
+	 * Receives the size of the array and constructs an array with exactly that number of elements.
+	 * @param capacity
+	 * @throws IllegalArgumentException
+	 */
 	@SuppressWarnings("unchecked")
 	public ArrayList(int capacity) throws IllegalArgumentException {
 		if (capacity < 0) {
 			throw new IllegalArgumentException("Initial capacity must be non-negative number.");
 		}
-		this.arr = (T[]) new Object [capacity];
+		this.array = (T[]) new Object [capacity];
 		this.capacity = capacity;
 		this.size = 0;
 	}
 	
+	
+	/**
+	 * @return Number of elements stored in the container.
+	 */
 	public int size() {
 		return this.size;
 	}
 	
+	
+	/**
+	 * @return Whether the container is empty.
+	 */
 	public boolean empty() {
 		return this.size == 0;
 	}
 
+	
+	/**
+	 * Empty the content of the container.
+	 */
 	public void clear() {
 		for (int i=0; i<size; ++i) {
-			arr[i] = null;
+			array[i] = null;
 		}
 		size = 0;
 	}
 	
+	
+	/**
+	 * Exchanges the contents of two containers.
+	 * @param other - Another ArrayList with the same type of data stored in.
+	 */
 	public void swap(ArrayList<T> other) {
-		T[] tempArr = this.arr;
-		this.arr = other.arr;
-		other.arr = tempArr;
+		T[] tempArr = this.array;
+		this.array = other.array;
+		other.array = tempArr;
 
 		int tempCapacity = this.capacity;
 		this.capacity = other.capacity;
@@ -50,53 +86,70 @@ public class ArrayList <T> {
 		other.size = tempSize;
 	}
 	
-	public void swap(ArrayList<T> other) {
-		
-	}
 	
+	/**
+	 * @return The size of the underlying buffer.
+	 */
 	public int capacity() {
 		return this.capacity;
 	}
 	
+	
+	/**
+	 * Changes the capacity of the array, keeping the stored data.
+	 * @param newCapacity - The desired new capacity.
+	 */
 	@SuppressWarnings("unchecked")
-	private void changeCapacity(int newCapacity) throws IllegalArgumentException {
-		if (newCapacity < this.size) {
-			throw new IllegalArgumentException("Invalid new capacity value.");
-		}
+	private void changeCapacity(int newCapacity) {
 		T[] newArr = (T[]) new Object[newCapacity];
-		for (int i=0; i<this.size; ++i) {
-			newArr[i] = arr[i];
+		int newSize = Math.min (newCapacity, this.size);
+		for (int i=0; i<newSize; ++i) {
+			newArr[i] = array[i];
 		}
-		this.arr = newArr;
+		this.array = newArr;
 		this.capacity = newCapacity;
+		this.size = newSize;
 	}
 	
+	
+	/**
+	 * Ensures that the underlying buffer has at least a certain capacity.
+	 * @param capacity
+	 */
 	public void reserve(int capacity) {
 		if (this.capacity < capacity) {
 			changeCapacity(capacity);
 		}
 	}
 	
-	public void resize(int newSize) throws IllegalArgumentException {
-		if (newSize < 0) {
+	
+	/**
+	 * Set the size of the capacity to a specified value. 
+	 * @param newCapacity - The desired new size of the capacity.
+	 * @throws IllegalArgumentException - If the desired new size
+	 * of the capacity is negative, throw an exception.
+	 */
+	public void resize(int newCapacity) throws IllegalArgumentException {
+		if (newCapacity < 0) {
 			throw new IllegalArgumentException("New size must be non-negative number");
 		}
-		
-		if (newSize > this.capacity) {
-			changeCapacity(newSize);
-		}
-		else if (newSize < this.size) {
-			for (int i=this.size-1; i>=newSize; --i) {
-				this.arr[i] = null;
-			}
-		}
-		this.size = newSize;
+		changeCapacity(newCapacity);
 	}
 	
+	
+	/**
+	 * Reduce memory usage by freeing unused memory (if possible).
+	 */
 	public void shrink_to_fit() {
 		changeCapacity(this.size);
 	}
 	
+	
+	/**
+	 * Append the specified element to the end of the list. 
+	 * @param newElement - element to be appended 
+	 * @return true
+	 */
 	public boolean add(T newElement) {
 		if (this.size + 1 > this.capacity) {
 			if (this.size > 0) {
@@ -106,36 +159,64 @@ public class ArrayList <T> {
 				changeCapacity(1);
 			}
 		}
-		arr[this.size] = newElement;
+		array[this.size] = newElement;
 		this.size++;
 		return true;
 	}
 	
+	
+	/**
+	 * Remove the element at the end of the array.
+	 * @throws ArrayIndexOutOfBoundsException - If the container
+	 * is empty, elements cannot be removed, throw an exception.
+	 */
 	public void remove() throws ArrayIndexOutOfBoundsException {
 		if (this.size == 0) {
 			throw new ArrayIndexOutOfBoundsException("Cannot remove an element from an empty array");
 		}
+		this.array[this.size-1] = null;
 		this.size--;
 	}
 	
+	
+	/**
+	 * @param index
+	 * @return The element at the specified position in the array.
+	 * @throws IndexOutOfBoundsException - If the given index is
+	 * invalid, throw an exception.
+	 */
 	public T get(int index) throws IndexOutOfBoundsException {
 		if (index < 0 || index >= this.size) {
 			throw new IndexOutOfBoundsException("Invalid index");
 		}
-		return this.arr[index];
+		return this.array[index];
 	}
 	
+	
+	/**
+	 * Replaces the element at the specified position in the list 
+	 * with the specified element.
+	 * @param index
+	 * @param newValue
+	 */
 	public void set(int index, T newValue) {
 		if (index < 0 || index >= this.size) {
 			throw new IndexOutOfBoundsException("Invalid index");
 		}
-		arr[index] = newValue;
+		array[index] = newValue;
 	}
 	
+	
+	/**
+	 * Concatenates the contents of another array to the current 
+	 * one. The other array is not changed by the operation.
+	 * @param other - An ArrayList of the same type to be
+	 * concatenated.
+	 */
 	public void concatenate(ArrayList<T> other) {
 		reserve(this.size + other.size);
 		for (int i=0; i<other.size; ++i) {
-			arr[this.size + i] = other.get(i);
+			array[this.size + i] = other.get(i);
 		}
 		this.size += other.size;
 	}
